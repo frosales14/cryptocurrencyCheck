aa50eb63fb378c7ecab1686d06f40092ee77493c
Fernando Rosales <rosalesvegafernando@gmail.com>
2023-11-23 00:14:01 -0600

    FEAT: return state on useSelectMonedas and change styles

--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,25 +1,44 @@
 import styled from "@emotion/styled";
+import { useState } from "react";
 
 const Label = styled.label`
   color: #fff;
+  display: block;
+  font-family: "Lato", sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+  margin: 15px 0;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  font-size: 18px;
+  padding: 14px;
+  border-radius: 10px;
 `;
 
 const useSelectMonedas = (label, options) => {
+  const [state, setState] = useState("");
+
+  const handleSelectChange = (event) => {
+    setState(event.target.value);
+  };
+
   const SelectMonedas = () => (
     <>
       <Label>{label}</Label>
-      <select>
+      <Select value={state} onChange={handleSelectChange}>
         <option value="">Seleccione</option>
         {options.map((option) => (
           <option key={option.id} value={option.id}>
             {option.name}
           </option>
         ))}
-      </select>
+      </Select>
     </>
   );
 
-  return [SelectMonedas];
+  return [state, SelectMonedas];
 };
 
 export default useSelectMonedas;
